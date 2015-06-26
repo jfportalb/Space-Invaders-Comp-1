b84930bb65c360d0f5deee6af9cea18f3f5968ee
João <jfportalb@gmail.com>
2015-06-26 17:28:25 -0300

    Real first commit

---
+++ space.c
@@ -0,0 +1,19 @@
+/* Versão 0.2 do jogo Space Invaders
+ *
+ * Autor: João Felipe Porto de Albuquerque
+ * Data: 25/06/2015
+ */
+
+#include "jogo.h"
+
+int main(){
+	Jogo jogo;
+
+	inicializa_jogo(&jogo, 640, 480);
+
+	main_loop_jogo(&jogo);
+
+	destroi_jogo(&jogo);
+
+	return 0;
+}
